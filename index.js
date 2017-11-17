2b3e8ec0fc8995ef0445f2cd6c5c6d45a9f62360
alex <observable@163.com>
2017-11-17 17:30:25 +0800

    使用drawImage将一个canvas绘制到另一个canvas上

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var canvas = document.createElement('canvas');
 canvas.height = 200;
 canvas.width = 200;
 
-
 var ctx = canvas.getContext('2d');
 
 var grd = ctx.createRadialGradient(100, 100, 1, 100, 100, 100);
@@ -20,4 +19,15 @@ ctx.fillStyle = grd;
 ctx.fillRect(0, 0, 200, 200);
 
 
-document.body.appendChild(canvas);
+
+// 使用drawImage将一个canvas绘制到另一个canvas上
+var mainCanvas = document.createElement('canvas');
+mainCanvas.height = 300;
+mainCanvas.width = 300;
+
+var mainCtx = mainCanvas.getContext('2d');
+
+mainCtx.drawImage(canvas, 0, 0);
+
+
+document.body.appendChild(mainCanvas);
